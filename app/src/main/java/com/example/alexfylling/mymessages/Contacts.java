228b8842eff349cb94d85a8071b028bacefe2e62
alexfylling <alexfylling@gmail.com>
2016-09-16 12:50:46 +0200

    More on contacts & sorting. started on permission

---
+++ app/src/main/java/com/example/alexfylling/mymessages/Contacts.java
@@ -0,0 +1,82 @@
+package com.example.alexfylling.mymessages;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Contacts extends AppCompatActivity {
+    // The ListView
+    private ListView listNames;
+
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        this.listNames = (ListView) findViewById(R.id.listContacts);
+        listContacts();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void listContacts(){
+        // Android version is lesser than 6.0 or the permission is already granted.
+        List<String> contacts = getContactNames();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
+        listNames.setAdapter(adapter);
+    }
+
+    /**
+     * Read the name of all the contacts.
+     *
+     * @return a list of names.
+     */
+    private List<String> getContactNames() {
+        List<String> contacts = new ArrayList<>();
+        // Get the ContentResolver
+        ContentResolver cr = getContentResolver();
+        // Get the Cursor of all the contacts
+        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+        // Move the cursor to first. Also check whether the cursor is empty or not.
+        if (cursor.moveToFirst()) {
+            // Iterate through the cursor
+            do {
+                // Get the contacts name
+                String hasNumber = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER));
+                String name = "";
+
+                if (hasNumber.equals("1")){
+                    name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    if (!name.contains("@")) {
+                        contacts.add(name);
+                    }
+                }
+            } while (cursor.moveToNext());
+        }
+        // Close the cursor
+        cursor.close();
+        
+        Collections.sort(contacts);
+
+        return contacts;
+    }
+
+}
--- app/src/main/java/com/example/alexfylling/mymessages/MainActivity.java
+++ app/src/main/java/com/example/alexfylling/mymessages/MainActivity.java
@@ -1,45 +1,31 @@
 package com.example.alexfylling.mymessages;
 
-import android.Manifest;
-import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
 
     private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
-    // The ListView
-    private ListView listNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        this.listNames = (ListView) findViewById(R.id.listView);
+        // check in this activity and start a new activity with chat, or jump t a new activity for check and jump back to chat activity
         checkPermissionListContacts();
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -48,69 +34,22 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getBaseContext(), Contacts.class);
+                startActivity(intent);
             }
         });
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
     public void checkPermissionListContacts() {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{android.Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
             //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
         }
-        else {
-            // Android version is lesser than 6.0 or the permission is already granted.
-            List<String> contacts = getContactNames();
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
-            listNames.setAdapter(adapter);
-        }
 
     }
 
-
-    /**
-     * Read the name of all the contacts.
-     *
-     * @return a list of names.
-     */
-    private List<String> getContactNames() {
-        List<String> contacts = new ArrayList<>();
-        // Get the ContentResolver
-        ContentResolver cr = getContentResolver();
-        // Get the Cursor of all the contacts
-        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-
-        // Move the cursor to first. Also check whether the cursor is empty or not.
-        if (cursor.moveToFirst()) {
-            // Iterate through the cursor
-            do {
-                // Get the contacts name
-                String hasNumber = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER));
-                String name = "";
-
-                if (hasNumber.equals("1")){
-                    name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                    if (!name.contains("@")) {
-                        contacts.add(name);
-                    }
-                }
-            } while (cursor.moveToNext());
-        }
-        // Close the curosor
-        cursor.close();
-
-        return contacts;
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -121,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
             }
         }
+        //if (requestCode == ){}
     }
 
 }
